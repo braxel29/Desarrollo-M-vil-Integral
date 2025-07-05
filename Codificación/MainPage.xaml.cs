b6b32dbcbfe02e5cf8336d855ad75b026ca1e2f2
braxel29 <69501378+braxel29@users.noreply.github.com>
2025-07-04 21:27:40 -0600

    Add files via upload

---
+++ Codificación/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MauiFirebaseApp.Views;
+namespace MauiFirebaseApp
+{
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnAddProductClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AddProductPage());
+    }
+        
+
+    private async void OnSearchProductClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new SearchProductPage());
+    }
+
+    private async void OnListProductClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ListProductPage());
+    }
+
+
+
+
+}
+}
